c09a2564498185956a06b4de192bb1c62a516228
w.alexander <will_alexander@me.com>
2025-06-01 20:13:29 +0200

    Add Storybook to UI library

---
+++ libs/frontend/ui/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/angular';
+
+const config: StorybookConfig = {
+  stories: ['../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/angular',
+    options: {
+      builder: {
+        viteConfigPath: 'libs/frontend/ui/vite.config.mts',
+      },
+    },
+  },
+};
+
+export default config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/recipes/storybook/custom-builder-configs
